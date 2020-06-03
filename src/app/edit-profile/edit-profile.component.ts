d728ab3247f4e474ccebc5a9039c5486ffbb51fc
jemiluca <60889435+jemiluca@users.noreply.github.com>
2020-06-03 09:14:54 -0400

    right user name information updated

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
+  email: string;
+  firstName: string;
+  lastName: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+    this.email = this.authService.getEmail();
+    this.firstName = this.authService.getFirstName();
+    this.lastName = this.authService.getLastName();
   }
 
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
+  email: string;
+  firstName: string;
+  lastName: string;
+  constructor(
+    private authService: AuthenticationService
+  ) { }
   ngOnInit() {
+    this.email = this.authService.getEmail();
+    this.firstName = this.authService.getFirstName();
+    this.lastName = this.authService.getLastName();
   }
 
 }

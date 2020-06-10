213ca4f1f3c8ab6268a89d8fb5eb29589e645d14
r00tctrl <tulnat4@gmail.com>
2020-06-10 13:57:06 -0400

    showing actual volunteer numbers

---
+++ src/app/dashboard.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  constructor() { }
+}

cf62b6a4ccbda6d6cbf3ac0c047eb727f168bbd6
jemiluca <jemiluca123@gmail.com>
2020-07-17 20:37:59 -0400

    show request only for frofesstional and Auth guard updated

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -5,27 +5,28 @@ import { AuthenticationService } from './authentication.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate {
+    currentUser: any;
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (this.authenticationService.logged && 
+        if (this.authenticationService.logged &&
             !this.authenticationService.isTokenExpired()) {
             // // check if route is restricted by role
-            // if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
-            //     // role not authorised so redirect to home page
-            //     this.router.navigate(['/']);
-            //     return false;
-            // }
+            if (route.data.roles && route.data.roles.indexOf(this.currentUser.role) != -1) {
+                //     // role not authorised so redirect to home page
+                this.router.navigate(['/home']);
+                return false;
+                } else
 
-            // authorized so return true
-            return true;
+                // authorized so return true
+                return true;
+            }
+
+            // not logged in so redirect to login page with the return url
+            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+            return false;
         }
-
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return false;
     }
-}

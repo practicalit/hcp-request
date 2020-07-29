1fde5ca0b220d3abf11f2d153998a87a826a5b1c
r00tctrl <tulnat4@gmail.com>
2020-07-29 17:43:33 -0400

    making the comment to populate

--- src/app/pages/comment/comment.component.ts
+++ src/app/pages/comment/comment.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommentRequest } from '../../models/comment.model';
 import { RequestService } from '../../services/request.service';
 import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-comments',
@@ -25,25 +26,13 @@ export class CommentComponent implements OnInit {
     private formBuilder: FormBuilder,
     private requestService: RequestService,
     private activatedRoute: ActivatedRoute,
+    private authService: AuthenticationService
   ) { }
   request_id: number;
   requestedBy: string;
   ngOnInit(): void {
     this.comments = [];
-    this.activatedRoute.paramMap.subscribe(
-      params => {
-        this.request_id = Number(params.get('requestId'));
-        if (this.request_id != null && this.request_id) {
-          this.requestService.getComments(this.request_id).subscribe(response => {
-            if (response.success) {
-              this.comments = response.data;
-            }
-          }
-          )
-        }
-      }
-    );
-
+    this.loadComment();
     this.commentForm = this.formBuilder.group({
       comment: ['', Validators.required]
     });
@@ -57,18 +46,34 @@ export class CommentComponent implements OnInit {
       request_id: this.object_id,
       comment: this.commentForm.controls.comment.value
     };
-    this.requestService.postComment(request).subscribe(
+   this.requestService.postComment(request).subscribe(
       response => {
         if (response) {
-          this.message = "Successfully posted the comment";
-    let new_comment = [];
-          new_comment['comment'] = new_comment;
-          new_comment['first_name'] = [];
-          new_comment['last_name'] = [];
-           this.comments.unshift(new_comment);
+          this.message = "Successfully posted the comment.";
+          let new_comment = {};
+          new_comment['comment'] = this.commentForm.controls.comment.value;
+          new_comment['first_name'] = this.authService.getFirstName();
+          new_comment['last_name'] = this.authService.getLastName();
+          this.comments.unshift(new_comment);
 
         } else {
           this.message = "It didn't go through, please try again";
+        }
+      }
+    );
+  }
+  loadComment(){
+    this.activatedRoute.paramMap.subscribe(
+      params => {
+        this.request_id = Number(params.get('requestId'));
+        if (this.request_id != null && this.request_id) {
+          this.requestService.getComments(this.request_id).subscribe(response => {
+            if (response.success) {
+              console.log(response);
+              this.comments = response.data;
+            }
+          }
+          )
         }
       }
     );
